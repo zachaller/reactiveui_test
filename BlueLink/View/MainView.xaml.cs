c93048b7c0c384353f400916cc3a00d42b9afc49
Zach Aller <zachaller@hotmail.com>
2013-04-23 13:08:29 -0500

    init
    Signed-off-by: Zach Aller <zachaller@hotmail.com>

---
+++ BlueLink/View/MainView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using BlueLink.Resources;
+
+using ReactiveUI;
+using ReactiveUI.Routing;
+using ReactiveUI.Xaml;
+
+namespace BlueLink
+{
+	public partial class MainPage : PhoneApplicationPage, IViewFor<BlueLink.ViewModel.MainViewModel>
+    {
+        // Constructor
+        public MainPage()
+        {
+			ViewModel = new BlueLink.ViewModel.MainViewModel();
+            this.InitializeComponent();
+            this.Bind(ViewModel, x => x.CarName, x => x.textCarName.Text);
+
+			ViewModel.OkCommand.Subscribe(x => ViewModel.CarName = "what");
+
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (BlueLink.ViewModel.MainViewModel)value; }
+        }
+        public BlueLink.ViewModel.MainViewModel ViewModel { get; set; }
+
+        //public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register("ViewModel", typeof(MainViewModel), typeof(MainPage), new PropertyMetadata(null));
+
+        private void btnChangeCarName_Click(object sender, RoutedEventArgs e)
+        {
+            //ViewModel.SetCarName("Zach's Sonata");
+            ViewModel.CarName = "w";
+			ViewModel.OkCommand.Execute(ViewModel.CarName);
+        }
+    }
+}
---
+++ BlueLink/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ReactiveUI;
+using ReactiveUI.Routing;
+using ReactiveUI.Xaml;
+using ReactiveUI.Mobile;
+using ReactiveUI.Blend;
+
+namespace BlueLink.ViewModel
+{
+	public partial class MainViewModel : ReactiveObject
+    {
+        string _carName = "Car's Name";
+        public string CarName
+        {
+            get {
+                return _carName;
+            }
+            set {
+                this.RaiseAndSetIfChanged(ref _carName, value);
+            }
+        }
+
+        public ReactiveCommand OkCommand
+        {
+            get
+            {
+				return new ReactiveCommand(this.WhenAny(
+					x => x.CarName,
+					(x) => (
+						x.Value == "w"
+						)));
+            }
+        }
+
+		public MainViewModel()
+        {
+        }
+    }
+}
